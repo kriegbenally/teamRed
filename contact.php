4aed01f4c99f4558c79afcb0156866367b4354f4
SimeonAtcitty <atcittysimo@gmail.com>
2016-10-13 09:44:11 -0600

    added form to contact page
    added form to contact page

---
+++ contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Elegant Contact Form</h1>
+    <form class="cf">
+      <div class="half left cf">
+        <input type="text" id="input-name" placeholder="Name">
+        <input type="email" id="input-email" placeholder="Email address">
+        <input type="text" id="input-subject" placeholder="Subject">
+      </div>
+      <div class="half right cf">
+        <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
+      </div>
+      <input type="submit" value="Submit" id="input-submit">
+    </form>
+  </body>
+</html>
