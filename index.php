99d990fc1d7d7a85228948f0b7f0f655d222227c
hawkeyewoman <hawkeyewoman2013@gmail.com>
2016-10-13 10:17:39 -0600

    made changes again to navBar
    changes to index.php and navBar

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title></title>
     <?php require_once("lib/headutils.php"); ?>
+    <link rel="stylesheet" href="lib/css/index.css">
   </head>
   <body>
     <?php require_once("lib/partials/navbar.php");?>
