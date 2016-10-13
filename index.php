c10903615abed6664b9629af297d3736347f70e1
hawkeyewoman <hawkeyewoman2013@gmail.com>
2016-10-13 09:30:54 -0600

    addd index.php and more
    added index.php and lib folder containing the naBar

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <?php require_once("lib/headutils.php"); ?>
+  </head>
+  <body>
+    <?php require_once("lib/partials/navbar.php");?>
+
+  </body>
+</html>
